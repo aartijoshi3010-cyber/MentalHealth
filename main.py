adbebc81c065ec1884e53d278773f749c4377cb9
aartijoshi3010-cyber <aartijoshi3010@gmail.com>
2025-09-12 01:32:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -115,7 +115,7 @@ elif choice == "📚 Resources":
     - [📞 WHO Mental Health Support](https://www.who.int/news-room/fact-sheets/detail/mental-health-strengthening-our-response)
     """)
 
-    st.image("https://imperfect.co.in/support-systems-nurturing-mental/  🧠", use_column_width=True)
+    st.image("https://imperfect.co.in/support-systems-nurturing-mental/  🧠", use_container_width=True)
 
 # -------------------
 # Get Help
